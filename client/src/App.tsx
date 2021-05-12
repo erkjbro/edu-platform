b00d90b3c4091cffc21cb51e395beb7dfdd65f4f
Erik J Brown <erkjbro@gmail.com>
2021-05-12 17:25:01 -0500

    connecting client & api

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,26 @@
+import { useState, useEffect } from 'react';
+
 const App = () => {
+  const [msg, setMsg] = useState<string>('');
+  const [data, setData] = useState<any>({});
+
+  useEffect(() => {
+    (async () => {
+      const response = await fetch('http://localhost:5000/');
+      const { message, data } = await response.json();
+      setMsg(message);
+      setData({
+        ...data,
+      });
+    })();
+  }, []);
+
   return (
     <div>
       <h1>EDU Platform</h1>
+      {msg && <span>{msg}</span>}
+      <br />
+      {data && <span>{Object.values(data)}</span>}
     </div>
   );
 };
