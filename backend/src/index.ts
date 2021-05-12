f3e3d4677982c37193ca8d5be2629e39df05142f
Erik J Brown <erkjbro@gmail.com>
2021-05-12 11:31:09 -0500

    basic express setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,15 @@
-console.log('hi there');
+import express, { Request, Response, NextFunction } from 'express';
+
+const app = express();
+
+app.get('/', (req: Request, res: Response, next: NextFunction) => {
+  res.send(`
+  <div>
+    <h1>Hi there!</h1>
+  </div>
+  `);
+});
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000');
+});
