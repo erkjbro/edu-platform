4d3079b3ef30ef16858d27ee6e54dad615bc379e
Erik J Brown <erkjbro@gmail.com>
2021-05-13 15:58:07 -0500

    creating ts versions of user & course

--- backend/src/models/course.ts
+++ backend/src/models/course.ts
@@ -1,4 +1,33 @@
-// title
-// creator
-// skill level 'beginner / indermediate / advanced'
-// description
+import { model, Document, Model, Schema, Types } from 'mongoose';
+
+import { IUser } from './user';
+
+export interface ICourse extends Document {
+  title: string;
+  creator: IUser['_id'];
+  skillLevel: 'beginner' | 'intermediate' | 'advanced';
+  description: string;
+}
+
+const CourseSchema: Schema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  creator: {
+    type: Types.ObjectId,
+    required: true,
+  },
+  skillLevel: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+});
+
+const Course: Model<ICourse> = model('Course', CourseSchema);
+
+export default Course;
--- backend/src/models/user.ts
+++ backend/src/models/user.ts
@@ -1,6 +1,44 @@
-// name
-// email
-// password
-// role
-// courses
-// groups
+import { model, Document, Model, Schema, Types } from 'mongoose';
+
+export interface IUser extends Document {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  role: 'student' | 'teacher' | 'admin';
+}
+
+const UserSchema: Schema = new Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    required: true,
+  },
+  courses: [
+    {
+      type: Types.ObjectId,
+      ref: 'Course',
+      required: true,
+    },
+  ],
+});
+
+const User: Model<IUser> = model('User', UserSchema);
+
+export default User;
