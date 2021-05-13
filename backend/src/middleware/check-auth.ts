9f24528830a44754f1fe25c8d36c04b6d04e70cd
Erik J Brown <erkjbro@gmail.com>
2021-05-13 18:25:43 -0500

    early version of auth check middleware

--- backend/src/middleware/check-auth.ts
+++ backend/src/middleware/check-auth.ts
@@ -1,15 +1,40 @@
-// skip for OPTIONS
+import { RequestHandler } from 'express';
+import jwt from 'jsonwebtoken';
 
-// extract token
+import HttpError from '../models/http-error.js';
 
-// verify token exists
+const checkAuth = ((req: any, res, next) => {
+  if (req.method === 'OPTIONS') {
+    return next();
+  }
 
-// decode token
+  try {
+    // 1) Extract web token from request headers
+    const token = req.headers.authorization.split(' ')[1];
 
-// extract user data from token and store on request
-// include user role
-// req.userData = {};
+    // 2) Check that token exists
+    if (!token) {
+      throw new Error('Auth failed!');
+    }
 
-// if AuthN approved, continue
+    // 3) Decode token by verifying with jwt key
+    const decodedToken: any = jwt.verify(
+      token,
+      process.env.JWT_KEY as jwt.Secret
+    );
 
-// catch failures
+    // 4) Extract userId from decoded token
+    req.userData = {
+      userId: decodedToken.userId,
+      role: decodedToken.role,
+    };
+
+    // 5) Continue on to the routes requiring AuthN
+    next();
+  } catch (err) {
+    const error = new HttpError('Authentication failed!', 403);
+    return next(error);
+  }
+}) as RequestHandler;
+
+export { checkAuth };
