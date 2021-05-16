09c4739f69d2fc1a5d1d786793f9e41a1d17190e
Erik J Brown <erkjbro@gmail.com>
2021-05-16 00:03:10 -0500

    updated test logic

--- client/src/store/reducers/auth.test.ts
+++ client/src/store/reducers/auth.test.ts
@@ -1,37 +1,46 @@
-export {};
-// import { authReducer } from './auth';
-// import { ActionType } from '../action-types';
+import { authReducer } from './auth';
+import { ActionType } from '../action-types';
 
-// describe(authReducer, () => {
-//   it('should return the initial state', () => {
-//     expect(authReducer(undefined, {})).toEqual({
-//       token: null,
-//       userId: null,
-//       error: null,
-//       loading: false,
-//       authRedirectPath: '/'
-//     });
-//   });
+describe('auth reducer', () => {
+  it('should return the initial state', () => {
+    expect(
+      authReducer(undefined, {
+        type: ActionType.SET_AUTH_REDIRECT_PATH,
+        payload: { path: '/' },
+      })
+    ).toEqual({
+      token: null,
+      userId: null,
+      error: null,
+      loading: false,
+      authRedirectPath: '/',
+    });
+  });
 
-//   it('should store the token upon login', () => {
-//     expect(authReducer({
-//       token: null,
-//       userId: null,
-//       error: null,
-//       loading: false,
-//       authRedirectPath: '/'
-//     }, {
-//       type: ActionType.AUTH_SUCCESS,
-//       payload: {
-//         idToken: 'some-token',
-//         userId: 'some-user-id'
-//       }
-//     })).toEqual({
-//       token: 'some-token',
-//       userId: 'some-user-id',
-//       error: null,
-//       loading: false,
-//       authRedirectPath: '/'
-//     })
-//   })
-// });
+  it('should store the token upon login', () => {
+    expect(
+      authReducer(
+        {
+          token: null,
+          userId: null,
+          error: null,
+          loading: false,
+          authRedirectPath: '/',
+        },
+        {
+          type: ActionType.AUTH_SUCCESS,
+          payload: {
+            idToken: 'some-token',
+            userId: 'some-user-id',
+          },
+        }
+      )
+    ).toEqual({
+      token: 'some-token',
+      userId: 'some-user-id',
+      error: null,
+      loading: false,
+      authRedirectPath: '/',
+    });
+  });
+});
