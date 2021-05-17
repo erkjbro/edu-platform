c774f95d81e7130b9349980537dbd089357f4e9c
Erik J Brown <erkjbro@gmail.com>
2021-05-17 15:41:43 -0500

    ui component for user listing

---
+++ client/src/components/user/user-list/user-item/user-item.tsx
@@ -0,0 +1,24 @@
+import { Link } from 'react-router-dom';
+
+import { Card } from '../../../ui-kit';
+import './user-item.scss';
+
+const UserItem = (props: any) => {
+  const { _id, firstName, lastName, role, email } = props;
+
+  return (
+    <Card className='user__item'>
+      <Link to={`/user/${_id}`}>
+        <h2>
+          {firstName[0].toUpperCase() + firstName.slice(1)}
+          &nbsp;
+          {lastName[0].toUpperCase() + lastName.slice(1)}
+        </h2>
+        <h5>Role: {role[0].toUpperCase() + role.slice(1)}</h5>
+        <h5>E-Mail: {email}</h5>
+      </Link>
+    </Card>
+  );
+};
+
+export default UserItem;
---
+++ client/src/components/user/user-list/user-list.tsx
@@ -0,0 +1,16 @@
+import UserItem from './user-item/user-item';
+import './user-list.scss';
+
+const UserList = ({ users }: { users: any }) => {
+  return (
+    <ul className='user__list'>
+      {users.map((user: any) => (
+        <li key={user._id}>
+          <UserItem {...user} />
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default UserList;
