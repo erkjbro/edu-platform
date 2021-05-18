3cd6d04e8370d011c9db7d0f8c2ef899fe1d4329
Erik J Brown <erkjbro@gmail.com>
2021-05-17 22:35:03 -0500

    added login validation

--- backend/src/routes/auth-routes.ts
+++ backend/src/routes/auth-routes.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import { check } from 'express-validator';
 
 import * as authController from '../controllers/auth-controller.js';
 
@@ -8,6 +9,13 @@ router.post('/signup', authController.postSignup);
 
 router.post('/signup/:adminCode', authController.postSignup);
 
-router.post('/login', authController.postLogin);
+router.post(
+  '/login',
+  [
+    check('email').normalizeEmail().isEmail(),
+    check('password').trim().isAlphanumeric().not().isEmpty(),
+  ],
+  authController.postLogin
+);
 
 export { router as authRoutes };
